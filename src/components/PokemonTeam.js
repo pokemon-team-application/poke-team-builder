4283458f5a73d4bfd5304c32e30a458917ea31d5
Jonathan Azali <azali.jonathan@gmail.com>
2019-03-26 10:35:47 -0700

    full background implemented

--- src/components/PokemonTeam.js
+++ src/components/PokemonTeam.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "../styles/PokemonTeam.css";
+import React from 'react';
+import '../styles/PokemonTeam.css';
 
 class PokemonTeam extends React.Component {
   render() {
@@ -7,28 +7,28 @@ class PokemonTeam extends React.Component {
 
     if (pokemonList.length === 0) {
       return (
-        <section className="pokemon-team">
-          <h2 className="team-title">Team</h2>
+        <section className='pokemon-team'>
+          <h2 className='team-title'>Team</h2>
         </section>
       );
     }
 
     return (
-      <section className="pokemon-team">
-        <h2 className="team-title">Team</h2>
-        <div className="pokemon-team-list">
+      <section className='pokemon-team'>
+        <h2 className='team-title'>Team</h2>
+        <div className='pokemon-team-list'>
           {pokemonList.map(pokemon => (
-            <div key={pokemon.uuid} className="poke-bullets">
+            <div key={pokemon.uuid} className='poke-bullets'>
               <img
-                src="https://66.media.tumblr.com/1d710c53dddcd114b42e4781fa1c52cf/tumblr_inline_mi3fpcf9fz1roozkr.png"
-                alt="Poke Bullet"
+                src='https://66.media.tumblr.com/1d710c53dddcd114b42e4781fa1c52cf/tumblr_inline_mi3fpcf9fz1roozkr.png'
+                alt='Poke Bullet'
               />
-              <img src={pokemon.sprites.front_default} alt="Pokemon Sprite" />
+              <img src={pokemon.sprites.front_default} alt='Pokemon Sprite' />
               <span>{pokemon.name}</span>
               <span />
               <button
-                className="delete-button"
-                type="button"
+                className='delete-button'
+                type='button'
                 onClick={() => onRemovePokemon(pokemon.uuid)}
               >
                 x
