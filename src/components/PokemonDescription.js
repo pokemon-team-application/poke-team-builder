b6839d9dab73e98ee89948e78ddd5897fad325d3
Jonathan Azali <azali.jonathan@gmail.com>
2019-03-26 10:55:40 -0700

    styles added

--- src/components/PokemonDescription.js
+++ src/components/PokemonDescription.js
@@ -1,8 +1,8 @@
-import React from "react";
-import "../styles/PokemonDescription.css";
+import React from 'react';
+import '../styles/PokemonDescription.css';
 
 class PokemonDescription extends React.Component {
-  handleAdd = (event) => {
+  handleAdd = event => {
     event.preventDefault();
     this.props.handleAddPokemon();
   };
@@ -11,40 +11,42 @@ class PokemonDescription extends React.Component {
     const { currentPokemon } = this.props;
     if (!currentPokemon.id) {
       return (
-        <section className="pokemon-description">
-          <h2 className="pokemon-empty-header">Pokemon Description</h2>
+        <section className='pokemon-description'>
+          <h2 className='pokemon-empty-header'>Pokemon Description</h2>
         </section>
       );
     }
     return (
-      <section className="pokemon-description">
-        <header className="pokemon-description-header">
+      <section className='pokemon-description'>
+        <header className='pokemon-description-header'>
           <img
-            id="description-header-sprite"
+            id='description-header-sprite'
             src={currentPokemon.sprites.front_default}
-            alt="Pokemon Sprite"
+            alt='Pokemon Sprite'
           />
-          <h2 id="description-header-name">{currentPokemon.name}</h2>
+          <h2 id='description-header-name'>{currentPokemon.name}</h2>
           <button
-            id="description-header-add"
-            type="submit"
+            id='description-header-add'
+            type='submit'
             onClick={this.handleAdd}
           >
             +
           </button>
         </header>
-        <h3>Types</h3>
-        <ul>
-          {currentPokemon.types.map(type => (
-            <li key={type.type.name}>{type.type.name}</li>
-          ))}
-        </ul>
-        <h3>Abilities</h3>
-        <ul>
-          {currentPokemon.abilities.map(ability => (
-            <li key={ability.ability.name}>{ability.ability.name}</li>
-          ))}
-        </ul>
+        <div className='poke_info'>
+          <h3>Types</h3>
+          <ul>
+            {currentPokemon.types.map(type => (
+              <li key={type.type.name}>{type.type.name}</li>
+            ))}
+          </ul>
+          <h3>Abilities</h3>
+          <ul>
+            {currentPokemon.abilities.map(ability => (
+              <li key={ability.ability.name}>{ability.ability.name}</li>
+            ))}
+          </ul>
+        </div>
       </section>
     );
   }
